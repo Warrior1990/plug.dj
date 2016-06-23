956b07bd6233ccbf4237bf1946dc294f629863c1
warrior1990 <j6rgen.estonia@gmail.com>
2016-06-23 16:05:58 +0300

    Update basicbot.js

--- basicbot.js
+++ basicbot.js
@@ -47,13 +47,13 @@
     //Change the bots default settings and make sure they are loaded on launch
 
     localStorage.setItem("basicBotsettings", JSON.stringify({
-      botName: "basicBot",
+      botName: "ChilloutBot",
       language: "english",
       chatLink: "https://rawgit.com/basicBot/source/master/lang/en.json",
       scriptLink: "https://rawgit.com/basicBot/source/master/basicBot.js",
       roomLock: false, // Requires an extension to re-load the script
-      startupCap: 1, // 1-200
-      startupVolume: 0, // 0-100
+      startupCap: 200, // 1-200
+      startupVolume: 50, // 0-100
       startupEmoji: false, // true or false
       autowoot: true,
       autoskip: false,
@@ -106,13 +106,13 @@
       songstats: true,
       commandLiteral: "!",
       blacklists: {
-        NSFW: "https://rawgit.com/basicBot/custom/master/blacklists/NSFWlist.json",
-        OP: "https://rawgit.com/basicBot/custom/master/blacklists/OPlist.json",
-        BANNED: "https://rawgit.com/basicBot/custom/master/blacklists/BANNEDlist.json"
+        NSFW: null,
+        OP: null,
+        BANNED: null
       }
     }));
 
     // Start the bot and extend it when it has loaded.
-    $.getScript("https://rawgit.com/basicBot/source/master/basicBot.js", extend);
+    $.getScript("null", extend);
 
 }).call(this);
